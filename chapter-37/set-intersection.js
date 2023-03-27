89d0d9cae44dd46bbbf355aeed9808d299b71b6e
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-27 22:21:49 +0900

    Set 교집합

---
+++ chapter-37/set-intersection.js
@@ -0,0 +1,18 @@
+// 교집합 A∩B
+Set.prototype.intersection = function (set) {
+  const result = new Set();
+
+  for (const value of set) {
+    if (this.has(value)) result.add(value);
+  }
+  return result;
+};
+
+const setA = new Set([1, 2, 3, 4]);
+const setB = new Set([2, 4]);
+
+// setA와 setB의 교집합
+console.log(setA.intersection(setB)); // Set(2) { 2, 4 }
+
+// setB와 setA의 교집합
+console.log(setB.intersection(setA)); // Set(2) { 2, 4 }
