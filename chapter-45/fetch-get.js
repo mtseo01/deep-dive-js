298f29cca96070e49177a8a35e76a3de73374fbd
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-04-10 00:25:08 +0900

    45장 fetch (get / asycn await)

--- chapter-45/fetch-get.js
+++ chapter-45/fetch-get.js
@@ -1,4 +1,5 @@
 const fetchBtn = document.getElementById('fetch-btn');
+const fetchBtn2 = document.getElementById('fetch-btn2');
 
 fetchBtn.addEventListener('click', () => {
   fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -7,4 +8,17 @@ fetchBtn.addEventListener('click', () => {
     .catch((error) => {
       console.log(error);
     });
+});
+
+// async await 사용
+fetchBtn2.addEventListener('click', async () => {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/2'
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
