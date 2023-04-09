a46d0068742e655d186bbd061896664c9b9fbc24
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-04-10 00:26:01 +0900

    45장 fetch (post / async await)

--- chapter-45/fetch-post.js
+++ chapter-45/fetch-post.js
@@ -1,4 +1,5 @@
 const postBtn = document.getElementById('post-btn');
+const postBtn2 = document.getElementById('post-btn2');
 
 postBtn.addEventListener('click', () => {
   fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -14,4 +15,25 @@ postBtn.addEventListener('click', () => {
   })
     .then((response) => response.json())
     .then((json) => console.log(json));
+});
+
+// async await 사용
+postBtn2.addEventListener('click', async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
