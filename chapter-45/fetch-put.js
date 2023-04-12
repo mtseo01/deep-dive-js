763769f1305da3a976bacb8660cc8ae245222437
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-04-12 23:30:25 +0900

    45장 fetch (put / async await)

--- chapter-45/fetch-put.js
+++ chapter-45/fetch-put.js
@@ -1,4 +1,5 @@
 const updateBtn = document.getElementById('put-btn');
+const updateBtn2 = document.getElementById('put-btn2');
 
 updateBtn.addEventListener('click', () => {
   fetch('https://jsonplaceholder.typicode.com/posts/1', {
@@ -16,4 +17,29 @@ updateBtn.addEventListener('click', () => {
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
+});
+
+// async await 사용
+updateBtn2.addEventListener('click', async () => {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/1',
+      {
+        method: 'PUT',
+        body: JSON.stringify({
+          id: 1,
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
