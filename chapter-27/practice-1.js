364f00474233a3fed3ec3cc022f92a24ac5f2dfe
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-05-31 23:15:11 +0900

    배열 가공 연습2(#이 안 붙어 있는 문자열에 붙이기 + # 2개 이상인 경우 가공하기)

--- chapter-27/practice-1.js
+++ chapter-27/practice-1.js
@@ -8,4 +8,22 @@ const unitedArr = dummyArr.map((keyword) => {
   }
 });
 
-console.log(unitedArr);
+console.log(unitedArr); // [ '#테스트', '#태그입니다', '#공부', '#공스타', '#코딩구' ]
+
+const dummyArr2 = [
+  '테스트',
+  '##태그입니다',
+  '##공부',
+  '###공스타',
+  '####코딩구',
+];
+
+const unitedArr2 = dummyArr.map((keyword) => {
+  let modifiedKeyword = keyword;
+  while (modifiedKeyword.startsWith('#')) {
+    modifiedKeyword = modifiedKeyword.slice(1);
+  }
+  return `#${modifiedKeyword}`;
+});
+
+console.log(unitedArr2); // [ '#테스트', '#태그입니다', '#공부', '#공스타', '#코딩구' ]
