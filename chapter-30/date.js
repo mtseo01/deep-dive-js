50f41b6b26fb7644a1f4341d015fe760e29ab376
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-06-08 23:55:20 +0900

    30장 Date

---
+++ chapter-30/date.js
@@ -0,0 +1,10 @@
+// new Date()
+
+const date = new Date();
+console.log(date); // 2023-06-08T14:52:24.231Z 객체로 반환
+
+const date2 = Date();
+console.log(date2); // Thu Jun 08 2023 23:53:51 GMT+0900 (Korean Standard Time) 문자열로 반환
+
+const test = 'test';
+console.log(test);
